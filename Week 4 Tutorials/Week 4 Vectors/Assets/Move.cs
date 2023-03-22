55b57da1bee9b9c992762a23b43c12774540ae6a
Cooper <cooper.plath@my.jcu.edu.au>
2023-03-22 11:39:50 +1000

    Week 4 tutorials

---
+++ Week 4 Tutorials/Week 4 Vectors/Assets/Move.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    public GameObject goal;
+    Vector3 direction;
+    float speed = 2f;
+
+    void Start() {
+
+        // direction = goal.transform.position - transform.position;
+        
+    }
+
+    private void LateUpdate() 
+    {
+        direction = goal.transform.position - transform.position;
+        this.transform.LookAt(goal.transform.position);
+        if (direction.magnitude > 2)
+        {
+            Vector3 velocity = direction.normalized * speed * Time.deltaTime;
+            this.transform.position = this.transform.position + velocity;
+        }
+        
+    }
+}
